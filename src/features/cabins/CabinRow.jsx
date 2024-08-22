7aedbed80b976a6178dd9a2fe6e813970288367a
zain <zainrajpootfd@gmail.com>
2024-08-22 16:08:27 +0500

    add new features

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,7 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import styled from "styled-components";
+import { deleteCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -38,3 +41,39 @@ const Discount = styled.div`
   font-weight: 500;
   color: var(--color-green-700);
 `;
+
+function CabinRow({ cabin }) {
+  const {
+    id: cabinId,
+    name,
+    maxCapacity,
+    regularPrice,
+    discount,
+    image,
+  } = cabin;
+  const queryClient = useQueryClient();
+  const { isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success("cabin successfully deleted");
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+    },
+    onError: (err) => toast.error("cabin could not be deleted"),
+  });
+  return (
+    <TableRow role="row">
+      <Img src={image} />
+      <Cabin>{name}</Cabin>
+      <div>Fits upto {maxCapacity} guests</div>
+      <Price>${regularPrice}</Price>
+      <Discount>${discount}</Discount>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>
+        Delete
+      </button>
+    </TableRow>
+  );
+}
+
+export default CabinRow;
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,10 +1,21 @@
 import supabase from "./supabase";
 
+// getCabins from supabase
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
   if (error) {
     console.error(error);
     throw new Error("cabin could not be loaded");
+  }
+  return data;
+}
+
+// deleteCabin from ui
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("cabin could not be deleted");
   }
   return data;
 }
