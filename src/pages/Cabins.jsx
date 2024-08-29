e758acefc2f7de448e98ad0b6b1a3e4a26f5d945
zain <zainrajpootfd@gmail.com>
2024-08-29 22:51:35 +0500

    add more features

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
-import CreateCabinForm from "../features/cabins/CreateCabinForm";
-import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
         <p>Filter / Sort</p>
       </Row>
+
       <Row>
         <CabinTable />
-        <Button onClick={() => setShowForm((show) => !show)}>
-          Add new Cabin
-        </Button>
-        {showForm && <CreateCabinForm />}
+        <AddCabin />
       </Row>
     </>
   );
