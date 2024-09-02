cecc14f6e219c77e51961fa769b4b4311e34f85b
zain <zainrajpootfd@gmail.com>
2024-09-02 17:24:48 +0500

    add more feature

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,14 +1,17 @@
 import BookingRow from "./BookingRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import Empty from "../../ui/Empty";
+
 import { useBookings } from "./useBookings";
 import Spinner from "../../ui/Spinner";
-import Empty from "../../ui/Empty";
 import Pagination from "../../ui/Pagination";
 
 function BookingTable() {
   const { bookings, isLoading, count } = useBookings();
+
   if (isLoading) return <Spinner />;
+
   if (!bookings.length) return <Empty resourceName="bookings" />;
 
   return (
@@ -29,6 +32,7 @@ function BookingTable() {
             <BookingRow key={booking.id} booking={booking} />
           )}
         />
+
         <Table.Footer>
           <Pagination count={count} />
         </Table.Footer>
